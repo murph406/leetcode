b8f2c711b08a2a4c00460ac39195660b925797cb
Ryan Murphy <ryanmurphy@r.local>
2022-11-11 10:26:25 -0800

    palindrome-number solution

---
+++ palindrome-number.js
@@ -0,0 +1,30 @@
+/*
+Given an integer x, return true if x is a 
+palindrome, and false otherwise.
+*/
+
+
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+
+var isPalindrome = function (x) {
+    const numString = x.toString()
+    const charArr = numString.split('')
+    const reverseCharArr = charArr.slice().reverse()
+
+    for (let i = 0; i < charArr.length; i++) {
+        if (charArr[i] !== reverseCharArr[i]) {
+            return false
+        }
+    }
+
+
+    return true
+};
+
+const num = 121
+const bool = isPalindrome(num)
+
+console.log("Is num palindrome::", bool)
