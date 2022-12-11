88a075cb03902fbe1122f196456ce28c9d5af013
Ryan Murphy <ryanmurphy@r.local>
2022-12-10 16:49:07 -0800

    linked-list-cycle

---
+++ linked-list-cycle-two.js
@@ -0,0 +1,61 @@
+/**
+Given the head of a linked list, return the node where the cycle begins. If there is no cycle, return null.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to (0-indexed). It is -1 if there is no cycle. Note that pos is not passed as a parameter.
+
+Do not modify the linked list.
+
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+
+class LinkedList {
+    constructor(head = null) {
+        this.head = head
+    }
+}
+
+class ListNode {
+    constructor(value) {
+        this.value = value
+        this.next = null
+    }
+}
+
+const detectCycle = (head) => {
+    // let fast = head
+    // let slow = head
+
+    // while (fast && fast.next) {
+    //     fast = fast.next.next
+    //     slow = slow.next
+
+    //     if (fast === slow) return true
+    // }
+
+    return false
+};
+
+const list = new LinkedList()
+const node1 = new ListNode(3)
+const node2 = new ListNode(5)
+const node3 = new ListNode(10)
+const node4 = new ListNode(-1)
+const node5 = new ListNode(11)
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node1
+list.head = node1
+
+const res = detectCycle(list.head)
+
+console.log('\n', "RES::", res, '\n')
